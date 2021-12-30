5397489dbb1dba91e451b63550eec1abec19728d
hyl <heyulong@henrywaltz.cn>
2021-12-30 15:09:40 +0800

    second commit

--- src/main/java/com/hyl/git/GitTest.java
+++ src/main/java/com/hyl/git/GitTest.java
@@ -2,6 +2,7 @@ package com.hyl.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git! ");
+        System.out.println("hello git! 1");
+        System.out.println("hello git! 2");
     }
 }
